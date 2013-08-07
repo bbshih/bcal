14eed4b74be5e97e157022cc362c1915743a0275
Billy Shih <me@billyshih.com>
2013-08-07 10:47:46 -0700

    Initial rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bcal::Application.config.secret_key_base = '92d2ba5647f8ee1b666ca936e4bb4d01b9021575778e6ada3d9accd41e63fd9626b5d9ec868f0d0746e92c5b363efa15ed5a1ac5fe645c69d4e6aa92ada38684'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bcal::Application.config.session_store :cookie_store, key: '_bcal_session'
