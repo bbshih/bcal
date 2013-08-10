9db325f294f691110693f0ecd4cd080579159c61
Billy Shih <me@billyshih.com>
2013-08-09 22:45:15 -0700

    Removed unneeded migration

--- db/migrate/20130808060114_change_time_columns_to_datetime.rb
+++
@@ -1,8 +0,0 @@
-class ChangeTimeColumnsToDatetime < ActiveRecord::Migration
-  def change
-    remove_column :events, :start_time, :time
-    remove_column :events, :end_time, :time
-    add_column :events, :start_time, :datetime
-    add_column :events, :end_time, :datetime
-  end
-end
